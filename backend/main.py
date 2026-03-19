973c5fdeaf13c30cda07251f2b75a53536c6ce6a
jhap <jhapgutz09@gmail.com>
2026-03-19 14:48:51 +0800

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import database
+import models
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
